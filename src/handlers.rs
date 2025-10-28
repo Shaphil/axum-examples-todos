85b1d4461d35fe31a00059396c83d14bfb69577f
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2025-10-28 23:15:11 +0600

    Add handler `todos_create`

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,7 +1,9 @@
-use crate::models::{Db, Pagination};
-use axum::extract::{Query, State};
-use axum::response::IntoResponse;
+use crate::models::{CreateTodo, Db, Pagination, Todo};
 use axum::Json;
+use axum::extract::{Query, State};
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
+use uuid::Uuid;
 
 pub async fn todos_index(pagination: Query<Pagination>, State(db): State<Db>) -> impl IntoResponse {
     let todos = db.read().unwrap();
@@ -14,4 +16,18 @@ pub async fn todos_index(pagination: Query<Pagination>, State(db): State<Db>) ->
         .collect::<Vec<_>>();
 
     Json(todos)
+}
+
+pub async fn todos_create(
+    State(db): State<Db>,
+    Json(input): Json<CreateTodo>,
+) -> impl IntoResponse {
+    let todo = Todo {
+        id: Uuid::new_v4(),
+        text: input.text,
+        completed: false,
+    };
+
+    db.write().unwrap().insert(todo.id, todo.clone());
+    (StatusCode::CREATED, Json(todo))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 mod handlers;
 mod models;
 
-use handlers::todos_index;
-
 use crate::models::Db;
-use axum::{routing::get, Router};
+use axum::{routing::get, routing::post, Router};
+use handlers::todos_create;
+use handlers::todos_index;
 
 #[tokio::main]
 async fn main() {
     let db = Db::default();
 
-    let app = Router::new().route("/", get(todos_index)).with_state(db);
+    let app = Router::new()
+        .route("/", get(todos_index))
+        .route("/", post(todos_create))
+        .with_state(db);
     const HOST: &str = "127.0.0.1";
     const PORT: u16 = 3000;
     let address = format!("{}:{}", HOST, PORT);
