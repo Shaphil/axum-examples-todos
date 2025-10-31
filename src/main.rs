95e47c73e46d9679caf2560efac04dcce8d0ff37
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2025-10-31 20:55:16 +0600

    Add an authorization middleware

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 mod handlers;
 mod models;
 
+use axum::error_handling::HandleErrorLayer;
+use axum::http::StatusCode;
 use axum::{routing::get, routing::patch, Router};
 use handlers::{todos_create, todos_delete, todos_index, todos_update};
 use models::Db;
+use std::time::Duration;
+use tower::{BoxError, ServiceBuilder};
+use tower_http::trace::TraceLayer;
 
 #[tokio::main]
 async fn main() {
@@ -12,6 +17,22 @@ async fn main() {
     let app = Router::new()
         .route("/", get(todos_index).post(todos_create))
         .route("/todos/{id}", patch(todos_update).delete(todos_delete))
+        .layer(
+            ServiceBuilder::new()
+                .layer(HandleErrorLayer::new(|error: BoxError| async move {
+                    if error.is::<tower::timeout::error::Elapsed>() {
+                        Ok(StatusCode::REQUEST_TIMEOUT)
+                    } else {
+                        Err((
+                            StatusCode::INTERNAL_SERVER_ERROR,
+                            format!("Unhandled internal error: {error}"),
+                        ))
+                    }
+                }))
+                .timeout(Duration::from_secs(10))
+                .layer(TraceLayer::new_for_http())
+                .into_inner(),
+        )
         .with_state(db);
     const HOST: &str = "127.0.0.1";
     const PORT: u16 = 3000;
